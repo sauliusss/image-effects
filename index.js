8495386f9a8652fce213be93102781f4f4f2d817
saulius <sjsaulius@gmail.com>
2024-02-01 15:36:05 +0200

    draw crop and slice img

--- index.js
+++ index.js
@@ -12,9 +12,14 @@ class Cell {
     // same width and height from main effect Object
     this.width = this.effect.cellWidth;
     this.height = this.effect.cellHeight;
+    // add projectImage
+    this.image = document.querySelector("#projectImage");
   }
   //   custom draw method
   draw(context) {
+    // call build draw image method (galima be canvas width ir height, bet sitas budas duoda daugiausiai kontroles)
+    // po this.image eina sx, sy, sw, sh = source x, source y, source width, source height
+    context.drawImage(this.image, this.x, this.y, this.width, this.height, this.x, this.y, this.width, this.height, 0, 0, canvas.width, canvas.height);
     context.strokeRect(this.x, this.y, this.width, this.height);
   }
 }
@@ -23,6 +28,7 @@ class Effect {
     this.canvas = canvas;
     this.width = this.canvas.width;
     this.height = this.canvas.height;
+
     // Slice canvas into grid
 
     this.cellWidth = this.width / 30;
@@ -50,7 +56,8 @@ class Effect {
   }
   //   custom render method
   render(context) {
-    this.imageGrid.forEach((cell) => {
+    // auto generated index ( is cia galima padaryti visokiu variaciju)
+    this.imageGrid.forEach((cell, i) => {
       cell.draw(context);
     });
   }
